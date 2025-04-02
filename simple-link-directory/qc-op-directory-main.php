4b3f33446bd7f33e7ad1728465a0dad7814cc0b9
Sean Murphy <sean@iamseanmurphy.com>
2025-04-02 10:18:07 +0000

    8.5.6

--- simple-link-directory/qc-op-directory-main.php
+++ simple-link-directory/qc-op-directory-main.php
@@ -3,11 +3,11 @@
  * Plugin Name: Link Directory - Simple Link Directory
  * Plugin URI: https://wordpress.org/plugins/simple-link-directory
  * Description: Link Directory WordPress plugin to curate topic based link collections. Curate gorgeous Link Directory, Local Business Directory, Partners or Vendors Directory
- * Version: 8.5.5
+ * Version: 8.5.6
  * Author: Link Directory
  * Author URI: https://www.quantumcloud.com/products/simple-link-directory/
  * Requires at least: 4.6
- * Tested up to: 6.7.2
+ * Tested up to: 6.7
  * Text Domain: qc-opd
  * Domain Path: /lang/
  * License: GPL2
